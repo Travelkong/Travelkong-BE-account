19a4a242162d10f31099e83368ed708e0fac54b2
Tyler <sun1231010@gmail.com>
2024-09-30 02:39:40 +0700

    Updated .env's variables and added .env.example

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { Server } from "http"
 
 async function startServer(server: Server): Promise<void> {
-    const port: string | number = process.env.PORT ?? 8080
+    const port: string | number = process.env.API_PORT ?? 8080
     server.listen(port, () => console.log(`[server]: The server is running on port: ${port}`))
 }
 
